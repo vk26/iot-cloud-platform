3c457cf6e3bf0679aa87c01020a213eaab3cda28
Dmitry Belyavtsev <vkosmos26@yandex.ru>
2020-05-31 16:45:46 +0800

    Send telemetry to Kafka

--- cloud-gateway/app/roles/api.lua
+++ cloud-gateway/app/roles/api.lua
@@ -4,8 +4,29 @@ local log = require('log')
 local uuid = require('uuid')
 local os = require('os')
 
+local fiber = require('fiber')
+local tnt_kafka = require('kafka')
+local json = require('json')
+
 local err_vshard_router = errors.new_class("Vshard routing error")
 local err_httpd = errors.new_class("httpd error")
+
+local error_callback = function(err)
+    log.error("got error: %s", err)
+end
+local log_callback = function(fac, str, level)
+    log.info("got log: %d - %s - %s", level, fac, str)
+end
+
+local producer, err = tnt_kafka.Producer.create({
+    brokers = "localhost:9092", -- TODO: use ENV
+    options = {}, -- options for librdkafka
+    error_callback = error_callback, -- optional callback for errors
+    log_callback = log_callback, -- optional callback for logs and debug messages
+    -- default_topic_options = {
+    --     ["partitioner"] = "murmur2_random",
+    -- }, 
+})
 
 local function json_response(req, json, status)
     local resp = req:render({json = json})
@@ -26,6 +47,17 @@ local function device_unauthorized(req)
         info = "Unauthorized"
     }, 401)
     return resp
+end
+
+local function produce_telemetry_to_kafka(telemetry)
+    local message = json.encode(telemetry)
+    local err = producer:produce_async({
+        topic = "telemetry_stream",
+        value = message
+    })
+    if err ~= nil then
+        lor.error(err)
+    end
 end
 
 local function http_telemetry_add(req)
@@ -55,6 +87,19 @@ local function http_telemetry_add(req)
     --     return storage_error_response(req, resp.error)
     -- end
 
+    produce_telemetry_to_kafka({
+        client_id = 1,
+        device_id = telemetry.device_id,
+        device_type = "sensor_01",
+        telemetry_key = telemetry.telemetry_key,
+        telemetry_value_int = telemetry.telemetry_value,
+        treshold_exceeded = false,
+        region = "Central",
+        city = "Saint-Petersburg",
+        department = "Department_01",
+        timestamp = telemetry.timestamp
+    })
+
     return json_response(req, {info = "Successfully created"}, 201)
 end
 
@@ -65,7 +110,7 @@ local function init(opts) -- luacheck: no unused args
             'universe',
             nil, { if_not_exists = true }
         )
-    end
+    end   
 
     local httpd = cartridge.service_get('httpd')
 
@@ -82,6 +127,7 @@ local function init(opts) -- luacheck: no unused args
 end
 
 local function stop()
+    producer:close() 
 end
 
 local function validate_config(conf_new, conf_old) -- luacheck: no unused args
