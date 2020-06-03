0245c931cac96a79069900b32e0146ccac5f4ec4
Dmitry Belyavtsev <vkosmos26@yandex.ru>
2020-06-03 15:00:22 +0800

    Change generator sample data

--- data_warehouse/utils/Rakefile.rb
+++ data_warehouse/utils/Rakefile.rb
@@ -15,18 +15,18 @@ logger = Logger.new(STDOUT)
 namespace :kafka do
   task :produce_telemetry_sample do
     logger.info 'Start producing messages ...'
-    10.times do |i|
+    10_000.times do |i|
       message =  {
         client_id: 1,
-        device_id: %w[dev01 dev02 dev03 dev04].sample,
-        device_type: %w[sensor_01 sensor_02].sample,
+        device_id: "dev_#{rand(100) + 1}",
+        device_type: "sensor_#{rand(10) + 1}",
         telemetry_key: %w[temperature pressure].sample,
         telemetry_value_int: [*50..180].sample,
         treshold_exceeded: Faker::Boolean.boolean(true_ratio: 0.1),
-        region: 'Central',
-        city: %w[Saint-Petersburg Moscow Novosibirsk Krasnoyarsk].sample,
+        region: %w[Central Northwest South Ural Siberia far-East].sample,
+        city: %w[Saint-Petersburg Moscow Krasnodar Ekaterinburg Novosibirsk Krasnoyarsk Irkutsk Vladivostok].sample,
         department: %w[Department_01 Department_02 Department_03].sample,
-        timestamp: Time.now.utc.to_i # unixtime
+        timestamp: (Faker::Date.backward(days: 90) + [*8..20].sample.hours).utc.to_i # unixtime
       }.stringify_keys
       DeliveryBoy.deliver(message.to_json, topic: 'telemetry_stream')
     end
